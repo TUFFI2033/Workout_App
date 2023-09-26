7cea609516bbb3917d5cc5c385144ff06930d453
Ivan <bindiuua@gmail.com>
2023-09-26 21:54:48 +0100

    add

---
+++ WorkoutApp/Extensions/UIKit/UIView + Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIView + Extensions.swift
+//  WorkoutApp
+//
+//  Created by Ivan Byndiu on 04/09/2023.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addShadowOnView() {  
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        layer.shadowOpacity = 0.7
+        layer.shadowRadius = 1    }
+}
